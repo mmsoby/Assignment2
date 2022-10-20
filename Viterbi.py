29c9a53b68a5f007b1b174dee8e6a9237672a655
Mustafa Soby <mustafasoby@icloud.com>
2022-10-20 19:30:01 -0400

    Completed ability to read in training set and necessary information for the viterbi algorithm.

--- Viterbi.py
+++ Viterbi.py
@@ -1,63 +1,101 @@
 import sys
-import numpy as np
+
+
+class Word:
+    def __init__(self, word, tag):
+        self.word = word
+        self.ground_truth_tag = tag
+        self.believed_tag = None
+        self.potential_tags = []
 
 
 class Viterbi:
     def __init__(self, training_file):
         self.training_file = training_file
-        self.training_sentences, self.training_tags = Viterbi.preprocess(self.training_file)
-
-    @staticmethod
-    def preprocess(file):
-        temp_sentences, temp_tags = [[]], [[]]
-        # Get the training data line by line
-        with open(file, 'r') as f:
-            for line in f:
-                # Add a tag and word for the start of the sentence
-                temp_sentences.append('<s>')
-                temp_tags.append('<s>')
-
-                # Split the line into words and tags
-                words_tags = line.split()
-
-                # Split the words and tags into separate lists
-                words = [word_tag.split('/')[0] for word_tag in words_tags]
-                tags = [word_tag.split('/')[1] for word_tag in words_tags]
-
-                # Append the lists to the sentences and tags lists
-                temp_sentences.append(words)
-                temp_tags.append(tags)
-
-        return temp_sentences, temp_tags
+        self.tag_frequencies = {}  # Defined as {tag: frequency}
+        self.bigram_probabilities = {}  # Defined as {"Ti Tj": P(Tj|Ti) = count(Ti,Tj)/count(Ti)}
+        self.lexical_probabilities = {}  # Defined as {"Ti Wi": P(Wi|Ti) = count(Ti,Wi)/count(Ti)}
+        self.train()
+        self.accuracy = -1
 
     def train(self):
-        # Gather the necessary tag frequencies and bigram probabilities
-        pass
+        self.__get_tag_frequencies()
+        self.__get_bigram_probabilities()
+        self.__get_lexical_probabilities()
 
-    def __viterbi(self, sentence):
-        pass
+    def __get_lexical_probabilities(self):
+        lexical_counts = {}
+        # Grab tags line by line
+        with open(self.training_file, 'r') as f:
+            for line in f:
+                # Calculate lexical Counts
+                for tag_and_word in line.split(" "):
+                    if tag_and_word == "\n":
+                        continue
+                    if tag_and_word in lexical_counts:
+                        lexical_counts[tag_and_word] += 1
+                    else:
+                        lexical_counts[tag_and_word] = 1
+            # Calculate lexical probabilities
+            for tag_and_word in lexical_counts:
+                tag = tag_and_word.split("/")[1]
+                self.lexical_probabilities[tag_and_word] = lexical_counts[tag_and_word] / self.tag_frequencies[tag]
+
+    def __get_bigram_probabilities(self):
+        bigram_counts = {}  # Defined as {"Ti Tj": count(Ti,Tj)}
+        # Grab tags line by line
+        with open(self.training_file, 'r') as f:
+            for line in f:
+                tags = []
+                for word in line.split(" "):
+                    if word == "\n":
+                        continue
+                    tags.append(word.split('/')[1])
+                # Calculate bigram Counts
+                for i in range(len(tags)):
+                    if i == 0:
+                        bigram = "<s>/" + tags[i]
+                    else:
+                        bigram = tags[i - 1] + "/" + tags[i]
+                    if bigram in bigram_counts:
+                        bigram_counts[bigram] += 1
+                    else:
+                        bigram_counts[bigram] = 1
+
+        # Calculate bigram probabilities
+        for bigram in bigram_counts:
+            tag = bigram.split("/")[0]
+            self.bigram_probabilities[bigram] = bigram_counts[bigram] / self.tag_frequencies[tag]
+
+    def __get_tag_frequencies(self):
+        # Get frequency of each tag in the training data
+        self.tag_frequencies["<s>"] = 0
+        with open(self.training_file, 'r') as f:
+            for line in f:
+                self.tag_frequencies["<s>"] += 1
+                for word in line.split(" "):
+                    if word == "\n":
+                        continue
+                    tag = word.split('/')[1]
+                    if tag in self.tag_frequencies:
+                        self.tag_frequencies[tag] += 1
+                    else:
+                        self.tag_frequencies[tag] = 1
 
     def test(self, test_file):
-        testing_sentences, testing_tags = Viterbi.preprocess(test_file)
-
-        # From the start of this sentence to the end of this sentence
-        temp = np.array(testing_sentences)
-        sentences = np.split(temp, np.where(temp == '<s>')[0])
-        sentences = [item[1:] for item in sentences if len(item) > 1]
-
-        # Run viterbi on each sentence
-        for sentence in sentences:
-            self.__viterbi(sentence.tolist()[0])
+        pass
 
     def printAccuracy(self):
-        pass
+        if self.accuracy == -1:
+            print("Accuracy not calculated yet")
+        else:
+            print("Accuracy: " + str(self.accuracy))
 
 
 def main(argv):
     training_file = argv[1]
     testing_file = argv[2]
     viterbi = Viterbi(training_file)
-    viterbi.train()
     viterbi.test(testing_file)
     viterbi.printAccuracy()
 
