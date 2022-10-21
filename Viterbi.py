a83ffc425381bfe18956eb8095327ceabcc8d1ed
Mustafa Soby <mustafasoby@icloud.com>
2022-10-20 20:10:40 -0400

    Mid viterbi implementation

--- Viterbi.py
+++ Viterbi.py
@@ -2,11 +2,11 @@ import sys
 
 
 class Word:
-    def __init__(self, word, tag):
+    def __init__(self, word, ground_truth_tag):
         self.word = word
-        self.ground_truth_tag = tag
+        self.ground_truth_tag = ground_truth_tag
         self.believed_tag = None
-        self.potential_tags = []
+        self.potential_tags = [()]  # Defined as a list of tuples (tag_i-1, tag_i, score)
 
 
 class Viterbi:
@@ -16,7 +16,8 @@ class Viterbi:
         self.bigram_probabilities = {}  # Defined as {"Ti Tj": P(Tj|Ti) = count(Ti,Tj)/count(Ti)}
         self.lexical_probabilities = {}  # Defined as {"Ti Wi": P(Wi|Ti) = count(Ti,Wi)/count(Ti)}
         self.train()
-        self.accuracy = -1
+        self.count_of_correctly_labeled_tags = 0
+        self.count_of_tags = 0
 
     def train(self):
         self.__get_tag_frequencies()
@@ -82,14 +83,59 @@ class Viterbi:
                     else:
                         self.tag_frequencies[tag] = 1
 
+    def __viterbi(self, sentence):
+        Tags = list(self.tag_frequencies.keys())
+        # Initialization Step
+        for tag in Tags:
+            if tag == "<s>":
+                continue
+            word_given_tag = sentence[0].word + "/" + tag
+            if word_given_tag in self.lexical_probabilities:
+                tag_given_beginning = "<s>/" + tag
+                sentence[0].potential_tags.append(("<s>", tag, self.lexical_probabilities[word_given_tag] *
+                                                   self.bigram_probabilities[tag_given_beginning]))
+            else:
+                sentence[0].potenial_tags.append(("<s>", tag, 0))
+
+        # Iteration Step
+        for i in range(1, len(sentence)):
+            for tag in Tags:
+                if tag == "<s>":
+                    continue
+                word_given_tag = sentence[i].word + "/" + tag  # Time/NN
+                if word_given_tag in self.lexical_probabilities:
+                    max_score = 0
+                    max_tag = None
+                    for potential_tag in sentence[i - 1].potential_tags:  # For each tag in the previous word
+                        tag_given_tag = potential_tag[1] + "/" + tag  # potential_tag[1] = previous tag
+                        score = potential_tag[2] * self.lexical_probabilities[word_given_tag] * \
+                                self.bigram_probabilities[tag_given_tag]  # potential_tag[2] = previous score
+                        if score > max_score:
+                            max_score = score
+                            max_tag = potential_tag[1]
+                    sentence[i].potential_tags.append((max_tag, tag, max_score))
+                else:
+                    sentence[i].potential_tags.append((None, tag, 0))
+
     def test(self, test_file):
-        pass
+        # Iterate over each line in the file
+        with open(test_file, 'r') as f:
+            for line in f:
+                sentence = []
+                # Iterate over each word in the line
+                for phrase in line.split(" "):
+                    if phrase == "\n":
+                        continue
+                    word = phrase.split("/")[0]
+                    tag = phrase.split("/")[1]
+                    sentence.append(Word(word, tag))
+                self.__viterbi(sentence)
 
     def printAccuracy(self):
-        if self.accuracy == -1:
+        if self.count_of_tags == 0:
             print("Accuracy not calculated yet")
         else:
-            print("Accuracy: " + str(self.accuracy))
+            print("Accuracy: " + str(self.count_of_correctly_labeled_tags / self.count_of_tags))
 
 
 def main(argv):
